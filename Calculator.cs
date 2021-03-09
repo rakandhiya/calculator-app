0c39fca234b4eb6bcf7d8ded6c0dadd014bd691c
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2021-03-09 12:08:29 +0700

    implement menu system

--- Calculator.cs
+++ Calculator.cs
@@ -4,7 +4,46 @@ class Calculator
 {
     static void Main(string[] args)
     {
-        
+        Console.Title = "Aplikasi Calculator";
+
+        int selectMenu = SelectMenu();
+        if (selectMenu < 1 || selectMenu > 4)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Maaf, menu yang anda pilih tidak tersedia");
+            ExitProgram();
+        }
+
+        Console.WriteLine();
+
+        Console.Write("Inputkan nilai a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Inputkan nilai b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        if (selectMenu == 1) Console.WriteLine($"Hasil penambahan {a} + {b} = {Penambahan(a, b)}");
+        else if (selectMenu == 2) Console.WriteLine($"Hasil pengurangan {a} - {b} = {Pengurangan(a, b)}");
+        else if (selectMenu == 3) Console.WriteLine($"Hasil perkalian {a} * {b} = {Perkalian(a, b)}");
+        else Console.WriteLine($"Hasil pembagian {a} / {b} = {Pembagian(a, b)}");
+
+        ExitProgram();
+    }
+
+    static int SelectMenu()
+    {
+        Console.WriteLine("Pilih menu calculator");
+
+        Console.WriteLine();
+        Console.WriteLine("1. Penambahan");
+        Console.WriteLine("2. Pengurangan");
+        Console.WriteLine("3. Perkalian");
+        Console.WriteLine("4. Pembagian");
+
+        Console.WriteLine();
+        Console.Write("Pilih [1...4]: ");
+
+        return int.Parse(Console.ReadLine());
     }
 
     static int Penambahan(int a, int b)
@@ -25,5 +64,14 @@ class Calculator
     static double Pembagian(int a, int b)
     {
         return (double) a / b;
+    }
+
+    static void ExitProgram()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tekan sembarang key untuk keluar");
+        Console.ReadKey();
+
+        Environment.Exit(0);
     }
 }
